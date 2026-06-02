ea723169bcdb0189975bea78e2ce4a248c9e63a4
rotemgrossman1 <rotem.grossman@live.biu.ac.il>
2026-06-02 14:32:07 +0300

    Add capitalize function to format strings with proper casing

---
+++ js-and-algo/callbackAndArrow/arrows/ex5.js
@@ -0,0 +1,5 @@
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+console.log(capitalize("bOb")) // returns Bob
+console.log(capitalize("TAYLOR")) // returns Taylor
+console.log(capitalize("feliSHIA")) // returns Felishia
