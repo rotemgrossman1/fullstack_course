d3c165a8c8df6ac702c21612e620a055dcda6137
rotemgrossman1 <rotem.grossman@live.biu.ac.il>
2026-06-02 14:03:07 +0300

    Add responsive image gallery and JavaScript callback examples
    - Created an HTML file for an Arsenal-themed image gallery with Bootstrap for responsiveness.
    - Added CSS for styling the gallery layout, including responsive text sizes and image handling.
    - Implemented JavaScript callback functions demonstrating the use of higher-order functions.
    - Added a sandbox file with arrow functions for squaring a number and adding two numbers, along with a function to get a formal title.

---
+++ js-and-algo/callbackAndArrow/callbacks/ex1.js
@@ -0,0 +1,12 @@
+const push = function () {
+  console.log("pushing it!")
+}
+
+const pull = function () {
+  console.log("pulling it!")
+}
+const pushPull = function(somfunc){
+    somfunc()
+}
+pushPull(push) //should print "pushing it!"
+pushPull(pull) //should print "pulling it!"
---
+++ js-and-algo/callbackAndArrow/sandbox.js
@@ -0,0 +1,9 @@
+const square =(n)=>console.log(n*n)
+    const add = (x, y) => x + y
+
+    const getFormalTitle = (title, name)=>`${title} ${name}`
+square('4');
+console.log(add(4,5))
+
+formalTitle = getFormalTitle("Madamme", "Lellouche")
+console.log(formalTitle) //returns "Maddame Lellouche"
